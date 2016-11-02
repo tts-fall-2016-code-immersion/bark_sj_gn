41d33c53a1571194cc875e32f839971559c14367
shaunjthewiz <shaunjgov1@gmail.com>
2016-11-02 09:15:32 -0400

    worked on routing

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,32 @@
+class TweetsController < ApplicationController
+  def index
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to tweet_path(@tweet.id), notice: 'Welcome to the pound!'}
+      else
+        format.html { render :new, error: 'there was a problem'}
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
 
+   has_many :tweets
+
+
 
 end
