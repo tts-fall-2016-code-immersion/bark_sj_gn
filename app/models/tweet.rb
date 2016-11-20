c8d22aebc270544ea55f2e5c0a0f100a4cf308f8
shaunjthewiz <shaunjgov1@gmail.com>
2016-11-20 02:33:17 -0500

    styled home page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :likes
-  validates :message, presence: true, length: { maximum: 140, too_long: "A bark is only 140 characters! Everyone knows this mannn...!"}
+  validates :message, presence: true, length: { maximum: 150, too_long: "A bark is only 150 characters! Everyone knows this mannn...!"}
   default_scope -> { order(created_at: :desc) }
 end
